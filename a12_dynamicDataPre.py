ab112f8f12248d6b3db6a2a1154db81beb7ac298
moinabyssinia <michaelg.tadesse@gmail.com>
2021-10-28 18:50:47 -0400

    added dynamic well data maker

---
+++ a12_dynamicDataPre.py
@@ -0,0 +1,62 @@
+"""
+Created on Wed Aug 25 18:29:00 2021
+modified on thu oct 28 18:28:00 2021
+
+this program:
+*filters the withdrawal time series for each rowColPermit and
+ prepares it on a format that is needed for dfs0
+
+@author: Michael Getachew Tadesse
+
+"""
+import os
+import pandas as pd
+
+# change directory here to 'wellsByWatrshd' or 'wells_ag_lra_ByWatrshd'
+
+dirHome = "C:\\Users\\mtadesse\\Hazen and Sawyer\\MIKE_Modeling_Group - Documents\\"\
+        "ECFTX\\extractedWellData\\011-allECFTXPermits-county-useclass\\analysis\\"\
+                        "concat_refinedWells\\wellsByWatrshd"
+dirOut = "C:\\Users\\mtadesse\\Hazen and Sawyer\\MIKE_Modeling_Group - Documents\\"\
+        "ECFTX\\extractedWellData\\011-allECFTXPermits-county-useclass\\analysis\\"\
+                        "concat_refinedWells\\allUseClassWells4Dfs0"
+
+
+os.chdir(dirHome)
+
+wsList = os.listdir()
+
+for ws in wsList:
+    os.chdir(dirHome)
+
+    print(ws)
+
+    dat = pd.read_csv(ws)
+    dat.drop('Unnamed: 0', axis = 1, inplace = True)
+    
+    # modify month and year format
+    dat['date'] = pd.to_datetime(dat['date'])
+
+    print(dat[['date', 'id', 'withdrawal']])
+
+    rcpUnique = dat['id'].unique()
+    # print(rcpUnique)
+
+
+    dfs0File = pd.DataFrame(dat['date'].unique())
+    dfs0File.columns = ['date']
+
+    for rcp in rcpUnique:
+        df = dat[dat['id'] == rcp]
+        newDf = df[['date', 'withdrawal']]
+        newDf.columns = ['date', rcp]
+        # print(newDf)
+
+        # merge to rcpUnique
+        dfs0File = pd.merge(dfs0File, newDf, on="date", how="left")
+
+    print(dfs0File)
+
+    # save county dfs0 ready csv
+    os.chdir(dirOut)
+    dfs0File.to_csv(ws)
